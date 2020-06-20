aa0a68dfe3c62efffd96fd25a009141a71f11470
Jonathan Mai <23644860+TR1DENTSKY@users.noreply.github.com>
2020-06-20 10:35:43 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 import java.util.Scanner;
 
-/* This Banking Application will:
-   - Display "Welcome" message to the user with his or her name
-   - Display 5 options to the user (Check Balance, Deposit, Withdraw, Check Previous Transaction, Exit)
-   - Afterwards user will be allowed to make a choice
-   - Once made the corresponding functionality will be executed
-*/
-
 public class Main {
 
     public static void main(String[] args) {
